5ef86ad36b20a0401a16f712f3abdd18bbf8d18f
AlvaroRodriguezGallardo <80212790+AlvaroRodriguezGallardo@users.noreply.github.com>
2023-10-28 16:09:44 +0000

    Delete P2/e-commerce directory

--- P2/e-commerce/etienda/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("consultasP1/<int:number>/",views.consultasP1,name="consultasP1")
-]
